f28c5c11b3724cfbab89d22637ff1dd411e5c0fc
Kyungju <wwmix@naver.com>
2023-03-29 11:11:07 +0900

    리액트 라우터 설정 초기화

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home';
-import Root from './pages/Root';
-import Videos from './pages/Videos';
-import VideosDetail from './pages/VideosDetail';
+import { Outlet } from 'react-router-dom';
+
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Root />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: 'videos/:searchName', element: <Videos /> },
-        { path: 'videos/watch/:videoID', element: <VideosDetail /> },
-      ],
-    },
-  ]);
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
